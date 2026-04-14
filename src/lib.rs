183d38c27d6ca9035b59e1f511e9d4364df2ecae
haruki7049 <tontonkirikiri@gmail.com>
2026-04-15 08:36:22 +0900

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,6 @@ enum Route {
     #[layout(Navbar)]
     #[route("/")]
     Home {},
-    #[route("/blog/:id")]
-    Blog { id: i32 },
 }
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
@@ -46,32 +44,6 @@ fn Hero() -> Element {
 fn Home() -> Element {
     rsx! {
         Hero {}
-
-    }
-}
-
-/// Blog page
-#[component]
-fn Blog(id: i32) -> Element {
-    rsx! {
-        div {
-            id: "blog",
-
-            // Content
-            h1 { "This is blog #{id}!" }
-            p { "In blog #{id}, we show how the Dioxus router works and how URL parameters can be passed as props to our route components." }
-
-            // Navigation links
-            Link {
-                to: Route::Blog { id: id - 1 },
-                "Previous"
-            }
-            span { " <---> " }
-            Link {
-                to: Route::Blog { id: id + 1 },
-                "Next"
-            }
-        }
     }
 }
 
@@ -85,12 +57,7 @@ fn Navbar() -> Element {
                 to: Route::Home {},
                 "Home"
             }
-            Link {
-                to: Route::Blog { id: 1 },
-                "Blog"
-            }
         }
-
         Outlet::<Route> {}
     }
 }
